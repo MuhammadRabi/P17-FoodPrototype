7f722b1235b11ef82a664c02d3c3213120b8ee45
Muhammad Rabi <34184092+MuhammadRabi@users.noreply.github.com>
2021-02-11 05:29:55 -0800

    Add files via upload
    migrating the project to independent repo

---
+++ js/app.js
@@ -0,0 +1,63 @@
+// scroll to top functionality
+
+const scrollToTop = document.querySelector(".scroll-to-top");
+// Eventlistener when scrolling
+window.addEventListener("scroll", showScrollTop);
+
+// function to show the btn
+function showScrollTop() {
+  if (window.pageYOffset > 150) {
+    scrollToTop.classList.add("show");
+  } else {
+    scrollToTop.classList.remove("show");
+  }
+}
+
+// Eventlistener when click the button
+scrollToTop.addEventListener("click", goToTop);
+
+//function to got to top of page
+function goToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+// navigation functionality
+
+let navLinks = document.querySelectorAll(".navigation li a");
+
+function scrollToSection(element) {
+  element.forEach((link) =>
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      element.forEach((ele) => {
+        ele.classList.remove("active");
+      });
+      link.classList.add("active");
+      let section = document.querySelector(e.target.dataset.link).offsetTop;
+      window.scrollTo({
+        top: section,
+        left: 0,
+        behavior: "smooth",
+      });
+    })
+  );
+}
+// calling the navigation function
+scrollToSection(navLinks);
+
+// mini-navigatin on mobile screens
+
+const menuToggle = document.querySelector("nav .menu-toggle");
+const navigation = document.querySelector("nav .navigation");
+
+console.log(navigation);
+
+menuToggle.addEventListener("click", showNavigation);
+
+function showNavigation() {
+  navigation.classList.toggle("active");
+  menuToggle.classList.toggle("active");
+}
